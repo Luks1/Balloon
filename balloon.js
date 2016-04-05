e5073e7bd99868ad424a1ccb9410f846650b5a05
luxolo <luxolo@projectcodex.co>
2016-04-05 11:03:21 +0200

    my fisrt step

---
+++ balloon.js
@@ -0,0 +1,13 @@
+
+//store them in array
+var myOder = "4 sets of red, 3 sets of blue, 3 sets of yellow";
+// also here store in array
+var prices = "R4 for red, R5 for blue, R5.50 for yellow"
+// spliting my first line
+var fixedOnes = myOder.split(",");
+//spliting my second line
+var fixPr = prices.split(",");
+//printing out the first one
+console.log(fixPr);
+// printing out second one
+console.log(fixedOnes);
